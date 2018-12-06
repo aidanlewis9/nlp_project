a785152fd7afd660ecc5b03dae42794667d47cc4
Aidan Lewis <alewis9@nd.edu>
2018-12-06 17:35:24 -0500

    Added segment code

--- src/script.py
+++ src/script.py
@@ -1,30 +1,49 @@
+from segment import Segment
 from utilities.read_file import read_file
 from utilities.match import match
+from utilities.is_empty import is_empty
+
+from collections import defaultdict
 
 
 class Script:
     def __init__(self, path, regex):
         self.PATH_SUFFIX = "script.txt"
         self.EMPTY_STRING = ""
+        self.EXCLAMATION_POINT = '!'
         self.NEWLINE = '\n'
+        self.QUOTE = '"'
 
         self.regex = regex
         self.path = path + self.PATH_SUFFIX
 
         self.segments = self.segment_script()
+        self.dialogue = defaultdict(list)
 
     def segment_script(self):
         segments = list()
-        segment = self.EMPTY_STRING
+        segment = Segment()
 
         for line in read_file(self.path):
             if match(self.regex, line):
                 segments.append(segment)
-                segment = self.EMPTY_STRING
+                segment = Segment()
 
-            segment += line + self.NEWLINE
+            segment.add_line(line)
 
-        return segments[1:]
+        return segments
 
     def get_segments(self):
         return self.segments
+
+    def extract_dialogue(self):
+        for segment in self.segments:
+            lines = segment.get_lines()
+            n = len(lines)
+
+            # while i < 0:
+            #     if lines[i].isupper() and not is_empty(lines[i + 1]) \
+            #             and self.EXCLAMATION_POINT not in lines[i]:
+            #         segment.add_dialogue(lines[i], lines[i + 1])
+
+
---
+++ src/segment.py
@@ -0,0 +1,16 @@
+from collections import defaultdict
+
+
+class Segment:
+    def __init__(self):
+        self.lines = list()
+        self.dialogue = defaultdict(list)
+
+    def add_line(self, line):
+        self.lines.append(line)
+
+    def add_dialogue(self, character, quote):
+        self.dialogue[character].append(quote)
+
+    def get_lines(self):
+        return self.lines
