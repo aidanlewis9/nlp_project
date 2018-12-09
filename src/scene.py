c92134ce534a22923ed2cf5c49d3ee6512118153
Aidan Lewis <alewis9@nd.edu>
2018-12-08 21:53:24 -0500

    Changed scene

--- src/scene.py
+++ src/scene.py
@@ -11,9 +11,3 @@ class Scene:
 
     def add_dialogue(self, character, quote):
         self.dialogue[character].append(quote)
-
-    def get_lines(self):
-        return self.lines
-
-    def get_dialogue(self):
-        return self.dialogue
